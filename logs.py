2a9eba04a6dca8fc9c3841ff0a2916398c4d4f67
MA3CIN <marcin.zet@onet.pl>
2024-05-29 12:45:48 +0200

    sample logs to parse from s141

--- logs.py
+++ logs.py
@@ -1,6 +1,17 @@
-def logs_fun():
-    return ("ret from logs")
-
+import os
 
 # from /var/logs/pod or /containers get logs for given name (f5gc, o5gs)
 # filter for Warning/Errors
+
+def handle_single_log_file(fileName):
+    pass
+
+
+def check_logs(location = './test-logs/var/log/pods', core_type = 'open5gs'):
+    selectedFiles =  os.listdir(location)
+    for file in selectedFiles:
+        if core_type in file:
+            pass
+    pass
+
+check_logs()
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import logs
 import state 
 
-# structure this around 5 modules: state, logs, metrics, traces...? + something from Service Mesh, not sure what
+# structure this around 5 modules: state, logs, metrics, traces...? + something from Service Mesh, not sure what (comm with their API)
 
 print(logs.logs_fun())
 print(state.check_pod_state())
