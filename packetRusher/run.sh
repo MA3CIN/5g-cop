50212057a9c072bae8992a24e60e6df5cb504b9c
Marcin Ziółkowski <marcin.zet@onet.pl>
2024-07-01 20:13:08 +0200

    10 -> 11

--- packetRusher/run.sh
+++ packetRusher/run.sh
@@ -1,3 +1,5 @@
-./app ue-latency-interval -n 10 > results.txt
+./app ue-latency-interval -n 11 > results.txt
 cat results.txt | grep LATENCY
 cat results.txt  | grep LATENCY | cut --complement -c1-87 | cut --complement -c6-9
+
+# place this in /my5G-RANTester/cmd
